9ac5d53e952ada92e1e8f3359d14f89cd4641998
Kamalusta <kamalusta@gmail.com>
2024-01-26 11:59:24 -0800

    third

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,9 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
         print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("{}: {}".format(1, sys.argv[1]))
     elif len(sys.argv) > 1:
         print("{} arguments:".format(len(sys.argv)-1))
         for i in range(1, len(sys.argv)):
