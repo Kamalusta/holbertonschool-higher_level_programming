757d058a73ea58b5e1b983121f42126fdac71455
Kamalusta <kamalusta@gmail.com>
2024-01-26 05:21:54 -0800

    corrction

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b == 1:
-        return a
-    elif b < 0:
-        a = 1/a
-        b *= -1
-    scr = a
-    for i in range(1, b):
-        scr *= a
-    return scr
+    return a ** b
