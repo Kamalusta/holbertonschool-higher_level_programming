59efaef5107f608fd3dd0e2094a1b34b5017432f
Kamalusta <kamalusta@gmail.com>
2024-01-31 19:17:57 +0400

    first

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print(my_list[i], end='')
         print()
-        return 0
+        return x
     except IndexError:
         print()
         return i
