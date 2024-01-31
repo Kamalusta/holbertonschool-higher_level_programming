72f50f3baeae027f023ed7b98ea45fd9ca7c0f45
Kamalusta <kamalusta@gmail.com>
2024-01-31 16:53:36 +0400

    last

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,11 +4,12 @@ def roman_to_int(roman_string):
         return 0
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
+    prev = 0
     for i in roman_string:
         for k, v in romans.items():
             if i is k:
-                if v > res:
-                    res = v - res
-                else:
-                    res += v
+                res += v
+                if v > prev:
+                    res = res - (prev * 2)
+                prev = v
     return res
