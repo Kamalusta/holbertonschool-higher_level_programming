410a2244eac90273022d4609bf7f09309e4bfb24
Kamalusta <kamalusta@gmail.com>
2024-01-31 15:38:29 +0400

    eleventh

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    best = 0
+    key = ''
+    for k, v in a_dictionary.items():
+        if best < v:
+            best = v
+            key = k
+    return key
