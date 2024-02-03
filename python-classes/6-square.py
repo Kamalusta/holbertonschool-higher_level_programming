c525590b1ae488e6a5a3a016b63663d4822157aa
Kamalusta <kamalusta@gmail.com>
2024-02-03 15:06:53 +0400

    seventh last

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -43,4 +43,7 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                print("_" * self.__position[0] + "#" * self.__size)
+                if self.__position[1] > 0:
+                     print("#" * self.__size)
+                else:
+                    print(" " * self.__position[0] + "#" * self.__size)
