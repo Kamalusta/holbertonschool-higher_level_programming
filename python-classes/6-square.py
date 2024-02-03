897194cc5ed3617930a945c527a582524e0a6a80
Kamalusta <kamalusta@gmail.com>
2024-02-03 14:51:35 +0400

    seventh last

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,8 +5,8 @@
 class Square:
     '''bla bla bla'''
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         return self.__size * self.__size
