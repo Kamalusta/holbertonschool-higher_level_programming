170ea5cf9b56da8f410d512c47756e8dfb561d83
Kamalusta <kamalusta@gmail.com>
2024-02-05 00:06:35 +0400

    chaing

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -17,6 +17,6 @@ def text_indentation(text):
         print(text[i], end='')
         if text[i] in ['.', '?', ':']:
             print("\n")
-            if text[i+1] in [' ', \t]:
+            if text[i+1] in [' ', '\t']:
                 i += 1
         i += 1
