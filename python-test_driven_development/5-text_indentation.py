e64de583f2d589c5b6a1e33c1b2d4ca67eac8c14
Kamalusta <kamalusta@gmail.com>
2024-02-04 23:56:09 +0400

    chaing

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,23 +12,12 @@ def text_indentation(text):
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    for i in range(0, len(text)):
-        if text[i] in [".", "?", ":"]:
-            print(text[i], end="")
+    i = 0
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in ['.','?',':']:
             print("\n")
-        elif text[i] == " ":
-            for j in range(i, 0, -1):
-                if text[j] == " ":
-                    continue
-                elif text[j] in [".", "?", ":"]:
-                    break
-                else:
-                    print(text[i], end="")
-                    break
-        else:
-            print(text[i], end="")
+            i = i + 1
+        i += 1
+            
 
-
-if __name__ == "__main__":
-    text_indentation("Holberton School")
