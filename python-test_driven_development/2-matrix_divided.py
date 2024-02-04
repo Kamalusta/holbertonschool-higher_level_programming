8831a4075e4afd6288fb46d4203d8beb5c124c77
Kamalusta <kamalusta@gmail.com>
2024-02-04 22:36:34 +0400

    second

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,6 +5,7 @@ this module devide
 all matrix elements
 """
 
+
 def matrix_divided(matrix, div):
     """
     this function takes a matrix and a divider
@@ -20,10 +21,9 @@ def matrix_divided(matrix, div):
         row = []
         for j in i:
             if type(j) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
             else:
                 row.append(round(j / div, 2))
         new_matrix.append(row)
     return new_matrix
-   
-    
