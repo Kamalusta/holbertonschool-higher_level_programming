92d2072f7feeacb52117b83a1f08f1ae4ed6a0c9
Kamalusta <kamalusta@gmail.com>
2024-02-11 00:17:04 +0400

    last one

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,7 +7,7 @@ def pascal_triangle(n):
     lis = []
     row = 0
 
-    for i in range(1, n+2):
+    for i in range(1, n+1):
         cur = []
         for j in range(i):
             if j == 0 or j == i-1:
