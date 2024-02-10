46249ec6b1edca6569a4eb035f81799bc14c1456
Kamalusta <kamalusta@gmail.com>
2024-02-11 00:16:12 +0400

    last one

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''doc'''
+
+
+def pascal_triangle(n):
+    '''doc'''
+    lis = []
+    row = 0
+
+    for i in range(1, n+2):
+        cur = []
+        for j in range(i):
+            if j == 0 or j == i-1:
+                row = 1
+            else:
+                a = lis[i-2][j]
+                b = lis[i-2][j-1]
+                row = a + b
+            cur.append(row)
+        lis.append(cur)
+    return lis
