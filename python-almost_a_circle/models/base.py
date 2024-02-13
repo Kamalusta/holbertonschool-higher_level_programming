50bca010d27062698fc4c0c7046b02cb6de254f0
Kamalusta <kamalusta@gmail.com>
2024-02-13 15:14:33 +0400

    fifteenth

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''data to JSON'''
         if list_dictionaries is None or len(list_dictionaries) == 0:
