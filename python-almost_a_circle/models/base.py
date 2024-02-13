237ab68aa15c1acd09a9a86f5d65021d828d43be
Kamalusta <kamalusta@gmail.com>
2024-02-13 15:44:01 +0400

    fifteenth

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,5 +22,4 @@ class Base:
         '''data to JSON'''
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
