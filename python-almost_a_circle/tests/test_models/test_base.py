96350d1ad36aff961e2303f6611494660873f993
Kamalusta <kamalusta@gmail.com>
2024-02-18 13:48:00 +0400

    test cases

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 ''' test cases'''
-from models import base
+from models.base import Base
 import unittest
 
 
@@ -8,11 +8,11 @@ class test_base(unittest.TestCase):
     '''doc '''
 
     def test_base_id(self):
-        b1 = base.Base(1)
-        self.assertEqual(b1.id, 1)
+        b1 = Base(3)
+        self.assertEqual(b1.id, 3)
 
     def test_baseid_node(self):
-        b1 = base.Base()
+        b1 = Base()
         self.assertEqual(b1.id, 1)
 
 
