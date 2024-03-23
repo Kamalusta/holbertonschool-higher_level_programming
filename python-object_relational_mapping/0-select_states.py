276c6f566c3b79f1c8902401cf77964c2ccafd61
Kamalusta <kamalusta@gmail.com>
2024-03-23 07:55:35 +0400

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 this script retrive data form a database with MySQLdb modul
 """
 
@@ -10,7 +10,7 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     password=sys.argv[2], db=sys.argv[3])
+                         password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
