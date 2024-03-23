26330f05726ca454ae1a3ef60dfefcc2725a5c7d
Kamalusta <kamalusta@gmail.com>
2024-03-23 14:34:32 +0400

    2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-this script retrive data which starting with 'N'
+this script retrive data which name is in argument
 form a database with MySQLdb modul
 """
 
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
-                (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
