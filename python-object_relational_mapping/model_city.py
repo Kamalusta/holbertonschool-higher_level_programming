874d1d4845cd27137508a080e4792fd86ecd0934
Kamalusta <kamalusta@gmail.com>
2024-03-24 17:41:42 +0400

    last

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -13,5 +13,5 @@ class City(Base):
     
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=True)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
