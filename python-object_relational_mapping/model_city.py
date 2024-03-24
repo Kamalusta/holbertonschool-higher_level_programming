7f6c4364d654e9000cde6c5c4b564118a83ecf74
Kamalusta <kamalusta@gmail.com>
2024-03-24 17:51:21 +0400

    last

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,14 +2,16 @@
 """doc"""
 
 
-from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+Base = declarative_base()
 
 
 class City(Base):
     """doc"""
 
-    
     __tablename__ = 'cities'
     id = Column(
         Integer,
